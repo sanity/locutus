d522f05dbe50692ba65b2087c671d925b2da8e12
Ian Clarke <github@ian.33mail.com>
2020-10-17 23:01:20 -0500

    commit before move to EC

--- src/main/kotlin/locutus/tools/crypto/rsa/RSAKeyPair.kt
+++ src/main/kotlin/locutus/tools/crypto/rsa/RSAKeyPair.kt
@@ -5,6 +5,8 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.*
 import java.security.interfaces.*
 
+// TODO: Switch to using Elliptic Curve: https://docs.oracle.com/javacard/3.0.5/api/javacard/security/ECPrivateKey.html
+//  https://docs.oracle.com/en/java/javase/13/security/oracle-providers.html#GUID-091BF58C-82AB-4C9C-850F-1660824D5254
 data class RSAKeyPair(val public : RSAPublicKey, val private : RSAPrivateKey) {
     companion object {
 
