aca113b593c0f5d3abfcdbac2444e3f9bf3d7859
Ian Clarke <ian.clarke@gmail.com>
2021-03-25 13:15:59 -0500

    checkpoint

---
+++ src/main/kotlin/locutus/client/LargePostRetriever.kt
@@ -0,0 +1,10 @@
+package locutus.client
+
+import locutus.net.ConnectionManager
+import locutus.protocols.ring.RingProtocol
+
+class LargePostRetriever(val ringProtocol : RingProtocol) {
+    init {
+
+    }
+}
